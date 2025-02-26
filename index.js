e437b3cc2ae7188b709e6561566c2d76c57ff014
Arimathea-Suarez <ahsuarez@up.edu.ph>
2025-02-26 12:40:19 +0800

    Added comments

--- index.js
+++ index.js
@@ -14,15 +14,16 @@ import validator from 'validator';
 // The first funtion in generating a unique ID
 
 export function generateUniqueID(firstName, lastName) {
-    if (typeof firstName !== "string" || typeof lastName !== "string") {
+    if (typeof firstName !== "string" || typeof lastName !== "string") { // For the validation of strings
         return null;
     }
 
+    // To convert the firstName, lastLetter and uniqueString to be trimmed and lowercased
     const firstLetter = firstName.trim().charAt(0).toLowerCase();
     const lastLetter = lastName.trim().toLowerCase();
     const uniqueString = uuidv4().replace(/~/g, '').substring(0, 8)
 
-    return `${firstLetter}${lastLetter}${uniqueString}`;
+    return `${firstLetter}${lastLetter}${uniqueString}`; // To return the unique ID
 }
 
 
@@ -31,14 +32,16 @@ export function generateUniqueID(firstName, lastName) {
 
 // The second function to add an account
 export function addAccount(accountData) {
-    if (!Array.isArray(accountData) || accountData.length !== 4) {
+    if (!Array.isArray(accountData) || accountData.length !== 4) { // To ensure that the accountData is an array with 4 elements
         return false;
 
     }
 
+    // To classify into different variables
     const [firstName, lastName, email, age] = accountData;
 
 
+    // For the validation of each field
     if (
         typeof firstName !== "string" || firstName.trim() === "" ||
         typeof lastName !== "string" || lastName.trim() === "" ||
@@ -50,6 +53,7 @@ export function addAccount(accountData) {
         return false;
     }
 
+
     const uniqueID = generateUniqueID(firstName, lastName);
     if (!uniqueID) {
         return false;
@@ -57,7 +61,7 @@ export function addAccount(accountData) {
 
     const userData = `${firstName},${lastName},${email},${age},${uniqueID}\n`;
 
-
+    // To append the user data inside the 'users.txt'
     try {
         appendFileSync('users.txt', userData, 'utf8');
         return true;
@@ -68,8 +72,6 @@ export function addAccount(accountData) {
     }
 
 }
-
-
 
 
 //References:
